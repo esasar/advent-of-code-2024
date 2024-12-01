96357b8e3bc71ffd7f85f33dcc2b147b011d79db
Esa Särkelä <esa.sarkela@tuni.fi>
2024-12-01 09:06:39 +0200

    day 1

--- day1/day1.c
+++ day1/day1.c
@@ -8,56 +8,76 @@ typedef struct {
     int value;
 } Pair;
 
+int partition(int *array, int low, int high) {
+    int pivot = array[high];
+    int i = low;
+    int j = 0;
+    int temp = 0;
+
+    for (j = low; j < high; j++) {
+        if (array[j] <= pivot) {
+            temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+            i++;
+        }
+    }
+
+    temp = array[i];
+    array[i] = array[high];
+    array[high] = temp;
+
+    return i;
+}
+
+int quicksort(int *array, int low, int high) {
+    int pivot = 0;
+
+    if (low >= high || low < 0) {
+        return 0;
+    }
+
+    pivot = partition(array, low, high);
+
+    quicksort(array, low, pivot - 1);
+    quicksort(array, pivot + 1, high);
+}
+
 int main(void) {
+    // vars for part 1
     FILE *file;
     char filename[] = "input";
-    char line[256];
     int left[NUMBERS];
     int right[NUMBERS];
     int count = 0;
     int i = 0;
     int j = 0;
+    int temp = 0;
     int sum = 0;
 
+    // vars for part 2
     Pair pairs[NUMBERS];
+    Pair pair = {0, 0};
     int number = 0;
     int sim_score = 0;
-    int temp = 0;
 
+    // part 1 
     file = fopen(filename, "r");
     if (file == NULL) {
         printf("Error: Could not open file %s\n", filename);
         exit(1);
     }
-
     while (fscanf(file, "%d %d", &left[count], &right[count]) == 2) {
         count++;
         if (count >= NUMBERS) {
             break;
         }
     }
-
     fclose(file);
 
-    for (i = 0; i < NUMBERS; i++) {
-        for (j = i + 1; j < NUMBERS; j++) {
-            if (left[i] > left[j]) {
-                int temp = left[i];
-                left[i] = left[j];
-                left[j] = temp;
-            }
-        }
-    }
+    quicksort(left, 0, NUMBERS - 1);
 
-    for (i = 0; i < NUMBERS; i++) {
-        for (j = i + 1; j < NUMBERS; j++) {
-            if (right[i] > right[j]) {
-                int temp = right[i];
-                right[i] = right[j];
-                right[j] = temp;
-            }
-        }
-    }
+    quicksort(right, 0, NUMBERS - 1);
 
     for (i = 0; i < NUMBERS; i++) {
         sum += abs(left[i] - right[i]);
@@ -65,9 +85,11 @@ int main(void) {
 
     printf("part 1 answer: %d\n", sum);
 
+    // part 2
     for (i = 0; i < NUMBERS; i++) {
         number = left[i];
-        Pair pair = {number, 0};
+        pair.key = number;
+        pair.value = 0;
         for (j = 0; j < NUMBERS; j++) {
             if (left[i] == right[j]) {
                 pair.value++;
